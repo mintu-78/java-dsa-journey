d559c994829f043e84637f1afa09723bfabf34c9
Mintu <pmk80028@gmail.com>
2026-04-18 14:04:28 +0530

    Use variables to perform basic arithmetic operations in Java

---
+++ Variables.java
@@ -0,0 +1,37 @@
+public class Main{
+    public static void main (String[] args) {
+
+        // Variables
+
+    //     String name = "Mintu kumar";
+    //     int age = 20;
+    //     double price = 48;
+    //     int a = 25;
+    //     int b = 10;
+        
+    //     b = 20;
+    //     name = "ironman"; 
+
+
+    //     System.out.println(name);
+    //     System.out.println(age);
+    //     System.out.println(price);
+    //     System.out.println(a);
+    // }
+
+
+    int a = 10;
+    int b = 25;
+
+    int sum = a + b;
+    // System.out.println(sum);
+
+    int diff =  b - a;
+    // System.out.println(diff);
+
+    int mul = a * b;
+    System.out.println(mul);
+
+    }
+    
+}
