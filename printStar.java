fa81edae615972f6fc6d8ad232e068331c65587a
Mintu <pmk80028@gmail.com>
2026-04-18 12:37:14 +0530

    Add basic star pattern program in Java

---
+++ printStar.java
@@ -0,0 +1,9 @@
+class printStar {
+    public static void main(String args[]){
+
+        System.out.println("*");
+        System.out.println("**");   
+        System.out.println("***");
+        System.out.println("****");        
+    }
+}
