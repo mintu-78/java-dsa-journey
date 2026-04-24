39859f1d3f5a295389af0872f3e6289040ec3dd2
Mintu <pmk80028@gmail.com>
2026-04-24 09:21:33 +0530

    Update Java program to print numbers from 0 to 10 using while loop

--- PrintNo0To10.java
+++ PrintNo0To10.java
@@ -2,9 +2,15 @@ public class PrintNo0To10{
 
     public static void main(String arga []){
 
-        for( int counter = 0; counter <= 10; counter ++){
-             System.out.println(counter);
+        // for( int counter = 0; counter <= 10; counter ++){
+        //      System.out.println(counter);
+        // }
+
+
+           int i = 0;
+        while(i <=10){
+            System.out.println(i);
+            i ++;
         }
-       
     }
 }
