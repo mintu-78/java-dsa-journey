89f94fbcbed02195a50412a1ae913990fe8c8f53
Mintu <pmk80028@gmail.com>
2026-04-25 08:00:10 +0530

    Demonstrate difference between while and do-while loop using example

--- PrintNo0To10.java
+++ PrintNo0To10.java
@@ -14,11 +14,21 @@ public class PrintNo0To10{
         // }
 
 
-        int i = 0;
-        do {
-            System.out.println(i);
-            i ++;
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i ++;
 
-        }while(i <=10);
+        // }while(i <=10);
+
+
+        int i = 12;
+        while(i < 12){
+            System.out.println("DSA Master");
+        }
+
+        do{
+            System.out.println("DSA Master");
+        } while(i < 11 );
     }
 }
