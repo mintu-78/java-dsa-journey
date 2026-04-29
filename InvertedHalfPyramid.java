c31752583d539fe444128c255b438410263db3a8
Mintu <pmk80028@gmail.com>
2026-04-29 09:25:08 +0530

    Print rotated inverted half pyramid pattern in Java

--- InvertedHalfPyramid.java
+++ InvertedHalfPyramid.java
@@ -1,15 +1,40 @@
-import java.util.*;
-public class InvertedHalfPyramid{
+// import java.util.*;
+// public class InvertedHalfPyramid{
 
-    public static void main(String args []){
+//     public static void main(String args []){
+
+//         int n = 4;
+
+//         for(int i = n; i >= 1; i--){
+//             for(int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+    //    IINVERTED HALF PYRAMID(rotated by 180 deg)//
+
+
+
+    import java.util.*;
+    public class InvertedHalfPyramidby180deg{
+
+        public static void main(String args[]){
 
         int n = 4;
 
-        for(int i = n; i >= 1; i--){
-            for(int j = 1; j <= i; j++){
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-    }
-}
+     }
+ }
