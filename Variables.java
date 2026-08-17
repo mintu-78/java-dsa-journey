affc03602ca7db8beec009c7962e2546d1e5b3e5
Mintu <pmk80028@gmail.com>
2026-08-17 22:44:50 +0530

    Implement Java output using print, println, and newline character

--- Variables.java
+++ Variables.java
@@ -3,35 +3,10 @@ public class Variables{
 
         // Variables
 
-    //     String name = "Mintu kumar";
-    //     int age = 20;
-    //     double price = 48;
-    //     int a = 25;
-    //     int b = 10;
-        
-    //     b = 20;
-    //     name = "ironman"; 
+    System.out.println("Hello World with Java");
+    System.out.print("Hello World with Java\n");
+    System.out.print("Hello World With Java");
 
-
-    //     System.out.println(name);
-    //     System.out.println(age);
-    //     System.out.println(price);
-    //     System.out.println(a);
-    // }
-
-
-    int a = 10;
-    int b = 25;
-
-    int sum = a + b;
-    System.out.println(sum);
-
-    int diff =  b - a;
-    System.out.println(diff);
-
-    int mul = a * b;
-    System.out.println(mul);
-
-    }
+  }
     
 }
