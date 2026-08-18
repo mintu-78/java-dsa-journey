1943ac0e007f323126d80168ea6211f275bab8d3
Mintu <pmk80028@gmail.com>
2026-08-18 23:13:04 +0530

    Add basic star pattern using print statements

--- Variables.java
+++ Variables.java
@@ -3,9 +3,14 @@ public class Variables{
 
         // Variables
 
-    System.out.println("Hello World with Java");
-    System.out.print("Hello World with Java\n");
-    System.out.print("Hello World With Java");
+    // System.out.println("Hello World with Java");
+    // System.out.print("Hello World with Java\n");
+    // System.out.print("Hello World With Java");
+
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
 
   }
     
