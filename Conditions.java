aa78d0cd63c18bdd5012b8c14db6f9cc8391b54b
Mintu <pmk80028@gmail.com>
2026-08-21 22:37:15 +0530

    Add basic if-else example for even and odd number checking

--- Conditions.java
+++ Conditions.java
@@ -5,12 +5,22 @@ public class Conditions {
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
+    //     int age = sc.nextInt();
 
-    if(age > 18){
-        System.out.println("Adult");
-    }else {
-        System.out.println("Not Adult");
+    // if(age > 18){
+    //     System.out.println("Adult");
+    // }else {
+    //     System.out.println("Not Adult");
+    // }
+
+    // Example 2 //
+
+    int x = sc.nextInt();
+
+    if(x % 2 == 0){
+        System.out.println("Even");
+    }else{
+        System.out.println("odd");
     }
 
     sc.close();
