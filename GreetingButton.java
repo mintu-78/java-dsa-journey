68a9e665b5512410a842618b431a6ee37b141a78
Mintu <pmk80028@gmail.com>
2026-08-22 09:29:55 +0530

    Implement greeting button using if-else and switch-case

--- GreetingButton.java
+++ GreetingButton.java
@@ -6,26 +6,26 @@ public class GreetingButton{
         Scanner sc = new Scanner (System.in);
         int button = sc.nextInt();
 
-    //     if(button == 1){
-    //         System.out.println("Hello");
-    //     }else if(button == 2){
-    //         System.out.println("Namaste");
-    //     }else if(button == 3){
-    //         System.out.println("Bonjour");
-    //     }else {
-    //         System.out.println("Invalid Button");
-    //     }
+    //  if(button == 1){
+    //     System.out.println("Hello");
+    //  }else if (button == 2){
+    //     System.out.println("Namaste");
+    //  }else if (button == 3){
+    //     System.out.println("Bonjour");
+    //  }else{
+    //     System.out.println("Invalid Button");
+    //  }  
 
-          switch(button){
-            case 1: System.out.println("Hello");
-            break;
-            case 2: System.out.println("Namaste");
-            break;
-            case 3: System.out.println("Bonjour");
-            break;
-            default : System.out.println("Invalid Button");
-          }
-          sc.close();
+    switch(button){
+        case 1 : System.out.println("Hello");
+        break;
+        case 2 : System.out.println("Namaste");
+        break;
+        case 3 : System.out.println("Bonjour");
+        break;
+        default : System.out.println("Invalid Button");
+        }
+     sc.close();
     }
 
 
