11ce9108ae8fdb6fdddbf6b7b4a2154ee54fd7be
Mintu <pmk80028@gmail.com>
2026-08-23 21:52:31 +0530

    Print numbers from 0 to 10 using for loop

--- PrintNo0To10.java
+++ PrintNo0To10.java
@@ -2,33 +2,9 @@ public class PrintNo0To10{
 
     public static void main(String arga []){
 
-        // for( int counter = 0; counter <= 10; counter ++){
-        //      System.out.println(counter);
-        // }
+  //counter++ => counter = counter + 1 
 
-
-        //    int i = 0;
-        // while(i <=10){
-        //     System.out.println(i);
-        //     i ++;
-        // }
-
-
-        // int i = 0;
-        // do {
-        //     System.out.println(i);
-        //     i ++;
-
-        // }while(i <=10);
-
-
-        int i = 12;
-        while(i < 12){
-            System.out.println("Hello Java");
-        }
-
-        do{
-            System.out.println("Hello Java");
-        } while(i < 11 );
+for(int counter = 0; counter < 11; counter ++)
+    System.out.println(counter+" ");
     }
 }
