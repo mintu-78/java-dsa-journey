35de85492a2e7e804547a364b77f60919f899100
Mintu <pmk80028@gmail.com>
2026-08-24 21:34:23 +0530

    print numbers from 0 to 10 using i variable in for loop

--- PrintNo0To10.java
+++ PrintNo0To10.java
@@ -4,7 +4,11 @@ public class PrintNo0To10{
 
   //counter++ => counter = counter + 1 
 
-for(int counter = 0; counter < 11; counter ++)
-    System.out.println(counter+" ");
+// for(int counter = 0; counter < 11; counter ++)
+//     System.out.println(counter+" ");
+
+for(int i = 0; i < 11; i++){
+  System.out.println(i);
+}
     }
 }
