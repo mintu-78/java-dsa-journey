d0e06ba92bcd6f40fee46144aab6873c1c231ba5
Mintu <pmk80028@gmail.com>
2026-08-27 11:53:05 +0530

    Implement rectangular star pattern using nested for loops

--- Patterns.java
+++ Patterns.java
@@ -3,14 +3,14 @@ public class Patterns{
     
     public static void main (String agrs []){
 
-        int n = 4;
-        int m = 5;
+        int a = 4;
+        int b = 5;
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
+        for (int i =1; i <= a; i ++){
+            for(int j = 1; j <= b;  j ++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        }
     }
-}
