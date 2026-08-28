fe4036b8de83d8ff0bd3b5ad391db661bb2eeab2
Mintu <pmk80028@gmail.com>
2026-08-28 19:28:20 +0530

    Implement increasing star pattern with nested loops

--- Patterns.java
+++ Patterns.java
@@ -3,11 +3,21 @@ public class Patterns{
     
     public static void main (String agrs []){
 
-        int a = 4;
-        int b = 5;
+        // int a = 4;
+        // int b = 5;
 
-        for (int i =1; i <= a; i ++){
-            for(int j = 1; j <= b;  j ++){
+        // for (int i =1; i <= a; i ++){
+        //     for(int j = 1; j <= b;  j ++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+         int a = 5;
+        int b = 3;
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
                 System.out.print("*");
             }
             System.out.println();
